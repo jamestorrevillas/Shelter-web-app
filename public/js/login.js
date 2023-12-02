b0c6e376862188585d8433d8d2f193cc5bc0e004
TorrevillasJames <james.torrevillas@cit.edu>
2023-12-02 12:24:38 +0800

    On branch main
    Initial commit
    
    Changes to be committed:
    	new file:   .firebaserc
    	new file:   .gitignore
    	new file:   firebase.json
    	new file:   public/css/style.css
    	new file:   public/css/style_dashboard.css
    	new file:   public/images/background_home.png
    	new file:   public/images/image_home.png
    	new file:   public/images/logo.png
    	new file:   public/images/pet_icon.png
    	new file:   public/images/pet_icon2.png
    	new file:   public/js/login.js
    	new file:   public/js/register.js
    	new file:   public/pages/about.html
    	new file:   public/pages/authentication.html
    	new file:   public/pages/dashboard.html
    	new file:   public/pages/forgot_password.html
    	new file:   public/pages/home.html
    	new file:   public/pages/login.html
    	new file:   public/pages/pets.html
    	new file:   public/pages/register.html

---
+++ public/js/login.js
@@ -0,0 +1,49 @@
+// login.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCBuYPKdZmVNe8BNMClJlNcK_ZkZ89qh1Q",
+    authDomain: "furry-found.firebaseapp.com",
+    databaseURL: "https://furry-found-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "furry-found",
+    storageBucket: "furry-found.appspot.com",
+    messagingSenderId: "283444505486",
+    appId: "1:283444505486:web:b0f69ce6e33a28aa46d2df",
+    measurementId: "G-8W6BBZYCHZ"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+
+function loginUser(email, password) {
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            console.log("User logged in:", user);
+            alert("Login successful!");
+            // Redirect or perform any other actions after successful login
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.error(`Login failed with error (${errorCode}): ${errorMessage}`);
+            alert("Login failed. Please check your email and password.");
+        });
+}
+
+document.getElementById("login").addEventListener("click", function () {
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value;
+
+    loginUser(email, password);
+});
+
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        console.log("User is already signed in:", user);
+        // Redirect or perform any other actions for already signed-in users
+    } else {
+        console.log("No user is signed in.");
+    }
+});
---
+++ public/js/register.js
@@ -0,0 +1,52 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
+  import { getDatabase, set, ref } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
+  import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCBuYPKdZmVNe8BNMClJlNcK_ZkZ89qh1Q",
+    authDomain: "furry-found.firebaseapp.com",
+    databaseURL: "https://furry-found-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "furry-found",
+    storageBucket: "furry-found.appspot.com",
+    messagingSenderId: "283444505486",
+    appId: "1:283444505486:web:b0f69ce6e33a28aa46d2df",
+    measurementId: "G-8W6BBZYCHZ"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const database = getDatabase(app);
+  const auth = getAuth();
+
+  signUp.addEventListener('click', (e) => {
+
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    var sheltername = document.getElementById('sheltername').value;
+
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed up 
+            const user = userCredential.user;
+
+            set(ref(database, 'shelters/' + user.uid),{
+                email: email,
+                shelter_name: sheltername,
+                password: password
+            })
+            alert('user created!');
+        // ...
+        })    
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+
+            alert('errorMessage');
+            // ..
+        });
+  });   
