d434e450b93e262be95c7655735889465434eaea
TorrevillasJames <james.torrevillas@cit.edu>
2023-12-11 15:50:00 +0800

    commit

--- public/js/history.js
+++ public/js/history.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
-import { getDatabase, ref, onValue, get } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
 
 // Your web app's Firebase configuration
@@ -20,25 +20,23 @@ const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 const auth = getAuth();
 
-const petsRef = ref(database, 'pets');
-const adoptersRef = ref(database, 'adopters');
-const sheltersRef = ref(database, 'shelters');
-const applicationFormRef = ref(database, 'applicationform');
+const historyRef = ref(database, 'applicationform');
 
-function displayApplicationData() {
-    onValue(applicationFormRef, (snapshot) => {
-        const applicationsData = snapshot.val();
-        const applicationsTableBody = document.getElementById('table-body-below');
-        applicationsTableBody.innerHTML = '';
+function displayHistoryData() {
+    onValue(historyRef, (snapshot) => {
+        const historyData = snapshot.val();
+        const historyTableBody = document.getElementById('table-body-below');
+        historyTableBody.innerHTML = '';
 
         const loggedInShelterId = getLoggedInShelterId();
 
-        for (const applicationId in applicationsData) {
-            if (Object.hasOwnProperty.call(applicationsData, applicationId)) {
-                const application = applicationsData[applicationId];
+        for (const applicationId in historyData) {
+            if (Object.hasOwnProperty.call(historyData, applicationId)) {
+                const application = historyData[applicationId];
 
+                // Check if the application is linked with the current shelter user and has a status of "completed"
                 if (application.shelter_id && application.shelter_id === loggedInShelterId && application.status === 'COMPLETED') {
-                    displayApplicationDetails(application, applicationId);
+                    displayApplication(application, applicationId);
                 }
             }
         }
@@ -47,41 +45,32 @@ function displayApplicationData() {
 
 function getLoggedInShelterId() {
     const user = auth.currentUser;
-
     return user ? user.uid : null;
 }
 
-async function displayApplicationDetails(application, applicationId) {
+function displayApplication(applicationDetails, applicationId) {
     const tableBody = document.getElementById('table-body-below');
 
-    // Fetch adopter details
-    const adopterDetails = await fetchUserData(adoptersRef, application.adopter_id);
-    
-    // Fetch pet details
-    const petDetails = await fetchUserData(petsRef, application.pet_id);
+    const { pet_name, adopter_name, contact_number, address, status } = applicationDetails;
 
-    const { first_name, last_name, phone_number, address } = adopterDetails;
-    const { name: petName } = petDetails;
-
-    // Extract other application details
-    const { status } = application;
+    const petName = pet_name;
+    const adopterName = adopter_name;
+    const contactNumber = contact_number;
+    const adopterAddress = address;
+    const applicationStatus = status;
 
     const tableRow = document.createElement('tr');
 
-    const adopterNameCell = document.createElement('td');
-    adopterNameCell.textContent = first_name + ' ' + last_name;
-
-    const contactNumberCell = document.createElement('td');
-    contactNumberCell.textContent = phone_number;
-
-    const addressCell = document.createElement('td');
-    addressCell.textContent = address;
-
     const petNameCell = document.createElement('td');
     petNameCell.textContent = petName;
-
+    const adopterNameCell = document.createElement('td');
+    adopterNameCell.textContent = adopterName;
+    const contactNumberCell = document.createElement('td');
+    contactNumberCell.textContent = contactNumber;
+    const addressCell = document.createElement('td');
+    addressCell.textContent = adopterAddress;
     const statusCell = document.createElement('td');
-    statusCell.textContent = status;
+    statusCell.textContent = applicationStatus;
 
     // Add table cells to the table row
     tableRow.appendChild(petNameCell);
@@ -96,28 +85,13 @@ async function displayApplicationDetails(application, applicationId) {
     document.getElementById('search-bar').addEventListener('input', filterTable);
 }
 
-// Function to fetch user data from a specific node in the database
-async function fetchUserData(nodeRef, userId) {
-    try {
-        const snapshot = await get(nodeRef);
-        const userData = snapshot.child(userId).val();
-        if (userData) {
-            return userData;
-        } else {
-            throw new Error("User not found");
-        }
-    } catch (error) {
-        throw error;
-    }
-}
-
 function filterTable() {
     const searchInput = document.getElementById('search-bar').value.toLowerCase();
     const tableRows = document.querySelectorAll('.colored-row');
 
     tableRows.forEach(row => {
-        const adopterNameCell = row.querySelector('td:nth-child(2)');
-        const statusCell = row.querySelector('td:nth-child(5)');
+        const adopterNameCell = row.querySelector('td:nth-child(1)');
+        const statusCell = row.querySelector('td:nth-child(4)');
 
         const adopterNameMatch = adopterNameCell.textContent.toLowerCase().includes(searchInput);
         const statusMatch = statusCell.textContent.toLowerCase().includes(searchInput);
@@ -130,4 +104,4 @@ function filterTable() {
     });
 }
 
-displayApplicationData();
+displayHistoryData();
