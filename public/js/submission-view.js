283ed114206899cbf2a01a873e0f74ea3906ff14
TorrevillasJames <james.torrevillas@cit.edu>
2024-01-06 10:31:37 +0800

    push

--- public/js/submission-view.js
+++ public/js/submission-view.js
@@ -48,8 +48,8 @@ async function displayApplicationData(applicationId) {
         document.getElementById('PetGender').value = petData.gender || '';
         document.getElementById('PetType').value = petData.type || '';
         document.getElementById('PetBreed').value = petData.breed || '';
-        document.getElementById('PetAge').value = petData.age + ' year/s old' || '';
-        document.getElementById('PetWeight').value = petData.weight + ' kg/s' || '';
+        document.getElementById('PetAge').value = petData.age || '';
+        document.getElementById('PetWeight').value = petData.weight || '';
         document.getElementById('PetDateArrived').value = petData.dateArrived || '';
         document.getElementById('PetDaysAtShelter').value = petData.daysAtShelter + ' day/s' || '';
         document.getElementById('DateApplied').value = applicationData.date_applied || '';
